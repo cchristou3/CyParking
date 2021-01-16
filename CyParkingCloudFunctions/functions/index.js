90e7bd45e5885cb25dcefb4423e590f94a6d1e67
Charalambos <cchristou3@uclan.ac.uk>
2021-01-16 22:35:52 +0200

    Add cleanupUser cloud function: when a user gets deleted, all its references in the database get deleted afterwards.
    Signed-off-by: Charalambos <cchristou3@uclan.ac.uk>

--- CyParkingCloudFunctions/functions/index.js
+++ CyParkingCloudFunctions/functions/index.js
@@ -12,17 +12,14 @@ const cors = require('cors')({ origin: true }); // Required for HTTP functions
 const helpers = require('./helpers');
 const constants = require('./constants')
 
-// curl --header "Content-Type: application/json" \
-//   --request POST \
-//   --data '{"latitude":"30","longitude":"29"}' \
-//   http://localhost:5001/cyparking-537e0/us-central1/filterLocations
-//   https://us-central1-cyparking-537e0.cloudfunctions.net/filterLocations
-
-// Still facing the same problem: https://dev.to/kodekage/understanding-node-error-errhttpheaderssent-19af
+/**
+ * When invoked, queries all parking lots from the database and filters them
+ * based on the distance between the parking lot and the user.
+ * It then returns the filtered list to the client.
+ */
 exports.filterLocations = functions.https.onCall(async (data, context) => {
     console.log('Data => ' + data)
     console.log('Context => ' + context)
-    // ref: https://www.endyourif.com/cant-set-headers-after-they-are-sent/
     if (!data.latitude || !data.longitude) { // if at least one of the parameters is empty
         // Throwing an HttpsError so that the client gets the error details.
         throw new functions.https.HttpsError('invalid-argument', 'Missing one or both parameters: latitude, longitude');
@@ -36,7 +33,6 @@ exports.filterLocations = functions.https.onCall(async (data, context) => {
         .then(
             function (querySnapshot) {
                 querySnapshot.forEach(function (doc) {
-                    //throw new functions.https.HttpsError('internal', "Oops something went wrong: " + doc)
                     if (doc.exists) {
                         console.log(doc.id, " => ", doc.data());
                         if (helpers.nearbyUser(doc.data(), userLatitude, userLongitude)) {
@@ -47,13 +43,76 @@ exports.filterLocations = functions.https.onCall(async (data, context) => {
             }
         ).catch((e) => { throw new functions.https.HttpsError('internal', "Oops something went wrong: " + e) })
 
-    //throw new functions.https.HttpsError('internal', "Oops something: " + filteredReadResult)
-
     console.log(filteredReadResult)
 
     // Send the filtered parking back to the user.
     return JSON.stringify(filteredReadResult);
 })
+
+/**
+ * When a user deletes their account, clean up after them.
+ */
+exports.cleanupUser = functions.auth.user().onDelete(async (user) => {
+    console.log(user.email + ' is about to get deleted')
+    // Delete any feedback messages related to this user    
+    admin.firestore().collection(constants.FEEDBACK)
+        .where(constants.EMAIL, constants.EQUALS, user.email).get()
+        .then((querySnapshot) => {
+            querySnapshot.docs.forEach((doc) => {
+                console.log("Delete message: " + JSON.stringify(doc.data()))
+                doc.ref.delete();
+            })
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+
+    // Delete any bookings that the user created
+    admin.firestore().collection(constants.BOOKINGS)
+        .where(constants.BOOKING_USER_ID, constants.EQUALS, user.uid).get()
+        .then((querySnapshot) => {
+            querySnapshot.docs.forEach((doc) => {
+                console.log("User booking: " + JSON.stringify(doc.data()))
+                doc.ref.delete();
+            })
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+
+    // Delete any bookings that the user's lot was booked.
+    admin.firestore().collection(constants.BOOKINGS)
+        .where(constants.OPERATOR_ID, constants.EQUALS, user.uid).get()
+        .then((querySnapshot) => {
+            querySnapshot.docs.forEach((doc) => {
+                console.log("Owner booking: " + JSON.stringify(doc.data()))
+                doc.ref.delete();
+            })
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+
+
+    // // Delete any parking lots related to this user
+    admin.firestore().collection(constants.PARKING_LOTS).where(constants.OPERATOR_ID, constants.EQUALS, user.uid).get()
+        .then((querySnapshot) => {
+            querySnapshot.docs.forEach((doc) => {
+                console.log("Lot owner message: " + JSON.stringify(doc.data()))
+                doc.ref.delete();
+            })
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+
+    admin.firestore().collection(constants.USERS).doc(user.uid).delete()
+        .then(function (writeResult) {
+            console.log('Result: ' + writeResult)
+        });
+    console.log(user.email + ' got deleted')
+    return;
+});
 
 /* TODO: Complete after administrator's front-end is done.
 exports.notifyAdminnistrator = functions.firestore
